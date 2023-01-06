cd88f2e0730edbc58b1026b1c5939ae0e8b916eb
rstrzalkowski <resumc@wp.pl>
2023-01-06 21:43:28 +0100

    Created spring boot application with postgresql connection

---
+++ src/main/java/pl/rstrzalkowski/syllabus/SyllabusApplication.java
@@ -0,0 +1,13 @@
+package pl.rstrzalkowski.syllabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyllabusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyllabusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rstrzalkowski/syllabus/SyllabusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rstrzalkowski.syllabus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyllabusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
