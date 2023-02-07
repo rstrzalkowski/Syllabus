a9984832195c744d5dc8fa64e6009812ece5f864
rstrzalkowski <resumc@wp.pl>
2023-02-07 17:33:51 +0100

    Added swipe handling to show and hide sidebar

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {ThemeService} from "../../services/theme.service";
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
@@ -10,6 +10,7 @@ import {Router} from "@angular/router";
 export class SidebarComponent implements OnInit {
 
   hidden = true;
+  defaultTouch = {x: 0, y: 0, time: 0};
 
   constructor(public themeService: ThemeService,
               private authService: AuthService,
@@ -19,6 +20,33 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
     if (window.innerWidth > 1024) {
       this.hidden = false;
+    }
+  }
+
+  @HostListener('touchstart', ['$event'])
+  @HostListener('touchend', ['$event'])
+  handleTouch(event: { touches: any[]; changedTouches: any[]; type: string; timeStamp: number; }) {
+    let touch = event.touches[0] || event.changedTouches[0];
+    // check the events
+    if (event.type === 'touchstart') {
+      this.defaultTouch.x = touch.pageX;
+      this.defaultTouch.time = event.timeStamp;
+    } else if (event.type === 'touchend') {
+      let deltaX = touch.pageX - this.defaultTouch.x;
+      let deltaTime = event.timeStamp - this.defaultTouch.time;
+
+      // simulate a swipe -> less than 500 ms and more than 60 px
+      if (deltaTime < 500) {
+        // touch movement lasted less than 500 ms
+        if (Math.abs(deltaX) > 60) {
+          // delta x is at least 60 pixels
+          if (deltaX > 0) {
+            this.show();
+          } else {
+            this.hide();
+          }
+        }
+      }
     }
   }
 
